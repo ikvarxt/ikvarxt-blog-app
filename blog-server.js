348ee9bfae32c1cb2bb9f5da5da4cdc5854f07c2
ikvarxt <leonekola@outlook.com>
2024-02-17 23:11:45 +0800

    implement mongodb

--- blog-server.js
+++ blog-server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const logger = require('./modules/logger')
 const articleRouter = require('./routes/articles')
 
-mongoose.connect('mongodb://localhost/ikvarxt-blog', {})
+mongoose.connect('mongodb://localhost:27017/ikvarxt-blog', {})
 
 const app = express();
 const server = http.createServer(app);
@@ -17,17 +17,19 @@ app.set('view engine', 'ejs')
 
 // define the routes
 app.get('/', (req, res) => {
-  const articles = [{
+  const articles = [
+    {
     title: "hello my first blog post",
     description: "some desc",
     date: new Date(),
-  },
+    },
     {
       title: "hello my second blog post",
       description: "some desc",
       date: new Date(),
-    }]
-  res.render('articles/index', { articles: articles })
+    }
+  ]
+  res.render('articles/', { articles: articles })
 })
 app.get('/about', (req, res) => {
   res.send('<h1>Hello, this is my about Page</h1>')
