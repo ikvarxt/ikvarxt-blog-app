c33677566ae431ebe65b2d6343cbe45bf6f70e91
ikvarxt <leonekola@outlook.com>
2024-02-18 20:07:04 +0800

    feat: show all articles in main page

--- blog-server.js
+++ blog-server.js
@@ -2,6 +2,7 @@ const http = require('http')
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./modules/logger')
+const Article = require('./modules/article')
 const articleRouter = require('./routes/articles')
 
 mongoose.connect('mongodb://localhost:27017/ikvarxt-blog', {})
@@ -16,21 +17,11 @@ app.use('/articles', articleRouter)
 app.set('view engine', 'ejs')
 
 // define the routes
-app.get('/', (req, res) => {
-  const articles = [
-    {
-    title: "hello my first blog post",
-    description: "some desc",
-    date: new Date(),
-    },
-    {
-      title: "hello my second blog post",
-      description: "some desc",
-      date: new Date(),
-    }
-  ]
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc'})
   res.render('articles/', { articles: articles })
 })
+
 app.get('/about', (req, res) => {
   res.send('<h1>Hello, this is my about Page</h1>')
 })
